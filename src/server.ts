c6a32328f92a410dc49352149aecb48e03483d84
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2022-08-04 14:46:50 -0300

    cors configuration

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import { AppError } from "./errors/AppError";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
